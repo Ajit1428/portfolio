8363b3e62ba61cc84931b8abc29386f5702b2dad
Ajit Thapa <ajitthapa248@gmail.com>
2024-08-13 09:32:52 +0545

    (fix): Minor fixes

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -30,7 +30,7 @@ export const HomePage = () => {
       <section className="flex flex-col items-center justify-center gap-20 xl:gap-[10rem] xl:flex-row">
         <div className="flex flex-col items-start gap-4 max-w-[50rem]">
           <span className="text-[22px] font-bold">Ajit Thapa</span>
-          <div className="font-bold text-[28px] text-light-primary xl:text-[3rem] dark:text-dark-primary">
+          <div className="font-bold text-[30px] text-light-primary xl:text-[3rem] dark:text-dark-primary">
             <ReactTyped
               strings={["Software Engineer", "A Web Developer"]}
               typeSpeed={100}
